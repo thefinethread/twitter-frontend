1ee72eb15577238ce2698d025c63fad68b9da05c
thefinethread <kartikey8604@gmail.com>
2024-06-16 23:08:16 +0530

    added image handling in post

--- components/Header/Index.vue
+++ components/Header/Index.vue
@@ -1,6 +1,6 @@
 <template>
 	<header
-		class="fixed left-1/4 right-[5%] xl:right-[30%] shadow-sm shadow-gray-700 top-0 h-14 z-20 backdrop-filter backdrop-blur bg-opacity-5 0"
+		class="fixed left-1/4 right-[5%] xl:right-[30%] shadow-sm shadow-gray-700 top-0 h-14 z-20 backdrop-filter backdrop-blur bg-opacity-50"
 	>
 		<slot />
 	</header>
--- components/Modal/Index.vue
+++ components/Modal/Index.vue
@@ -5,15 +5,18 @@
 	>
 		<div
 			@click.stop
-			class="bg-zinc-950 rounded-2xl p-5 w-[40rem] flex flex-col justify-between items-start translate-y-10"
+			class="bg-zinc-950 rounded-2xl p-5 w-[40rem] flex flex-col justify-between items-start mt-10 mb-10 max-h-[46rem]"
 		>
-			<button
-				@click="closeModal"
-				class="hover:bg-zinc-800 transition-colors p-1 rounded-full -translate-x-2 mb-6"
-			>
-				<XMarkIcon class="w-5" />
-			</button>
-			<div class="flex gap-3 w-full">
+			<div class="w-full pb-4 backdrop-blur-xl backdrop-filter bg-opacity-50">
+				<button
+					@click="closeModal"
+					class="hover:bg-zinc-800 transition-colors p-1 rounded-full"
+				>
+					<XMarkIcon class="w-5" />
+				</button>
+			</div>
+
+			<div class="flex gap-3 w-full min-h-24 py-2 overflow-y-scroll">
 				<img :src="GoogleLogo" alt="" class="w-10 h-10 rounded-full" />
 				<div class="flex-1">
 					<div
@@ -23,13 +26,14 @@
 						:data-placeholder="'What is happening?!'"
 					></div>
 
-					<div class="w-full my-7 relative">
+					<div class="w-full my-7 relative" v-if="imageUpload">
 						<img
-							:src="img"
+							:src="imageUpload"
 							alt=""
 							class="w-full my-7 rounded-2xl object-cover"
 						/>
 						<button
+							@click="removeImage"
 							class="bg-zinc-800 bg-opacity-70 hover:bg-zinc-800 absolute right-1 top-1 transition-colors p-1 rounded-full"
 						>
 							<XMarkIcon class="w-5" />
@@ -42,10 +46,16 @@
 
 			<div class="flex justify-between items-center w-full">
 				<ul>
-					<li>
-						<button>
+					<li class="relative">
+						<button class="cursor-pointer">
 							<PhotoIcon class="w-6 text-blue-400" />
 						</button>
+						<input
+							type="file"
+							class="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
+							@change="handleFileChange"
+							accept="image/*"
+						/>
 					</li>
 				</ul>
 
@@ -58,9 +68,9 @@
 <script setup>
 import { XMarkIcon, PhotoIcon } from '@heroicons/vue/24/outline';
 import GoogleLogo from '~/assets/images/Google.jpg';
-import img from '~/9.jpeg';
 
 const inputRef = ref(null);
+const imageUpload = ref(null);
 
 const { isModalOpen, closeModal } = defineProps(['isModalOpen', 'closeModal']);
 
@@ -72,6 +82,22 @@ const focusInput = () => {
 
 const handleOverlayClick = () => {
 	closeModal();
+};
+
+const handleFileChange = (event) => {
+	const file = event.target.files[0];
+
+	if (file) {
+		const reader = new FileReader();
+		reader.onload = (e) => {
+			imageUpload.value = e.target.result;
+		};
+		reader.readAsDataURL(file);
+	}
+};
+
+const removeImage = () => {
+	imageUpload.value = null;
 };
 
 onMounted(focusInput);
